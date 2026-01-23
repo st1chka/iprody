011df630656d1b900142dcd774dee8969741eeb3
st1chka <kalinine03@gmail.com>
2026-01-23 13:57:06 +0400

    create new module

---
+++ payment-service/payment-service-app/src/main/java/ru/iprody/paymentservice/paymentserviceapp/PaymentServiceAppApplication.java
@@ -0,0 +1,13 @@
+package ru.iprody.paymentservice.paymentserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceAppApplication.class, args);
+	}
+
+}
---
+++ payment-service/payment-service-app/src/test/java/ru/iprody/paymentservice/paymentserviceapp/PaymentServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iprody.paymentservice.paymentserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
