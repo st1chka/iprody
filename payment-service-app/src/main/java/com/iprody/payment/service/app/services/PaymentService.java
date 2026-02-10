f3117a1cd928d3bcc38369966308c913b1905c9e
st1chka <kalinine03@gmail.com>
2026-02-10 19:05:13 +0400

    Добавлена пагинация

--- payment-service-app/src/main/java/com/iprody/payment/service/app/services/PaymentService.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/services/PaymentService.java
@@ -1,10 +1,14 @@
 package com.iprody.payment.service.app.services;
 
-import com.iprody.payment.service.app.entity.Payment;
+import com.iprody.payment.service.app.entity.Payments;
+import com.iprody.payment.service.app.persistence.PaymentFilterFactory;
+import com.iprody.payment.service.app.persistence.records.PaymentFilter;
 import com.iprody.payment.service.app.repository.PaymentRepository;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +17,17 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
 
-    public List<Payment> getAllPayments() {
+    public List<Payments> getAllPayments() {
         return paymentRepository.findAll();
     }
 
-    public Payment getPaymentById(UUID id) {
+    public Payments getPaymentById(UUID id) {
         return paymentRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Payment not found by id: " + id));
+    }
+
+    public Page<Payments> searchPaged(PaymentFilter filter, Pageable
+            pageable) {
+        return paymentRepository.findAll(PaymentFilterFactory.fromFilter(filter), pageable);
     }
 }
